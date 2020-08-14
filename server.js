c52a0de55877758a536fe5764286bfa01a208f95
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-14 12:38:09 -0500

    required mongoos and morgan in server.js and created models folder

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+
+// requires the content in the models folder
+const db = require("./models");
 
 const app = express();
 
@@ -17,7 +22,7 @@ app.get("/exercise", (req, res) => {
 });
 // route for the stats page
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/stats.html"));
+	res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
 app.listen(PORT, () => {
