e1dc135f3bcf7c756265799bbfbb9a2d0d5b0f19
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-14 11:48:17 -0500

    added routes for other html pages

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const router = require("express").Router();
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+router.get("/exercise", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
