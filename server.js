df8cc44af36171ba3b6b3c39ab94748a0ba4cfe7
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-14 11:52:29 -0500

    added pseudocode to server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-// const router = require("express").Router();
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,12 +8,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// route that calls the the home page
 app.use(express.static("public"));
 
+// route for the exercise page
 app.get("/exercise", (req, res) => {
 	res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
-
+// route for the stats page
 app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
