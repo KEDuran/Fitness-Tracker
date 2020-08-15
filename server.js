7392b72a3e0531e43d79e360c1fcd83b530985e2
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-14 22:28:21 -0500

    changes patch back to put

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/api/workouts", (req, res) => {
 });
 
 // Route to update workout data - referenced activity 14
-app.patch("/api/workouts/:id", ({ body }, res) => {
+app.put("/api/workouts/:id", ({ body }, res) => {
 	db.Workout.create(body)
 		.then(({ _id }) =>
 			db.Workout.findOneAndUpdate(
